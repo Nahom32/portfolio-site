a69c11c0c616a61f03cdb9528848f579ca92f702
Nahom32 <senaynahom00@gmail.com>
2024-02-01 09:17:33 +0300

    added code for header

--- frontend/react_portfolio/src/App.js
+++ frontend/react_portfolio/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import Header from './components/header';
 
 function App() {
   return (
-    
+      <div>
+      <Header></Header>
       <LandingPage></LandingPage>
+      </div>
    
   );
 }
